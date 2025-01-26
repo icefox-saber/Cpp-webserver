3238a7ba9adbb57875b87cb8cf55967211cb2290
icefox-saber <728862050a@gmail.com>
2025-01-26 17:36:57 +0800

    tcpClient class

---
+++ Client/tcpClient.cpp
@@ -0,0 +1,54 @@
+#include "tcpClient.h"
+
+#define BUFFER_SIZE 1024
+
+tcpClient::tcpClient() { buffer = new char[BUFFER_SIZE]; };
+
+int tcpClient::initialize() {
+  return client_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+}
+int tcpClient::close() { return ::close(client_socket); };
+
+int tcpClient::connect(const std::string &ip, const uint16_t &port) {
+  return connect(ip.c_str(), port);
+}
+
+int tcpClient::connect(const char *ip, const uint16_t &port) {
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(port);
+  server_addr.sin_addr.s_addr = inet_addr(ip);
+  return ::connect(client_socket, (struct sockaddr *)&server_addr,
+                   sizeof(server_addr));
+}
+
+int tcpClient::send(const std::string &msg, size_t num) {
+  return send(msg.c_str(), num);
+}
+
+int tcpClient::send(const char *msg, size_t num) {
+  return ::send(client_socket, msg, num, 0);
+}
+
+int tcpClient::recv(size_t max_len) {
+  if (max_len > BUFFER_SIZE) {
+    max_len = BUFFER_SIZE;
+  }
+  return ::recv(client_socket, buffer, max_len, 0);
+}
+
+int tcpClient::getSocket() { return client_socket; }
+in_addr tcpClient::getServerAddr_int() { return server_addr.sin_addr; }
+
+std::string tcpClient::getServerAddr_string() {
+  return std::string(inet_ntoa(getServerAddr_int()));
+}
+
+char *tcpClient::getBuffer()
+{
+    return buffer;
+}
+
+tcpClient::~tcpClient() {
+  close();
+  delete buffer;
+}
---
+++ Client/tcpClient.h
@@ -0,0 +1,28 @@
+#include <arpa/inet.h>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <unistd.h>
+
+class tcpClient {
+private:
+  int client_socket;
+  sockaddr_in server_addr;
+  char *buffer;
+
+public:
+  tcpClient(/* args */);
+  int initialize();
+  int connect(const std::string &ip, const uint16_t &port);
+  int connect(const char *ip, const uint16_t &port);
+  int send(const std::string &msg, size_t n);
+  int send(const char *msg, size_t n);
+  int getSocket();
+  in_addr getServerAddr_int();
+  std::string getServerAddr_string();
+  int recv(size_t max_len);
+  int close();
+  char* getBuffer();
+  ~tcpClient();
+};
+
