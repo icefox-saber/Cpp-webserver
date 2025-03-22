d1217edf3439083d5d8a9c52089ab1ad45884b07
icefox-saber <728862050a@gmail.com>
2025-03-22 16:46:18 +0800

    blockqueue for async log

---
+++ include/blockqueue.h
@@ -0,0 +1,46 @@
+#include <condition_variable>
+#include <deque>
+#include <mutex>
+#include <queue>
+
+template <typename T> class blockqueue {
+private:
+  std::condition_variable productor_;
+  std::condition_variable consumer_;
+  std::mutex mtx_;
+  std::deque<T> deque_;
+  std::size_t capacity_;
+
+public:
+  void push_back(T &&msg);
+  void push_front(T &&msg);
+  void pop_front(T &dest);
+};
+
+template <typename T> void blockqueue<T>::push_back(T &&msg) {
+  {
+    std::unique_lock lck(mtx_);
+    productor_.wait(lck, [this]() { return deque_.size() < capacity_; });
+    deque_.push_back(std::forward<T>(msg));
+  }
+  consumer_.notify_one();
+}
+
+template <typename T> void blockqueue<T>::push_front(T &&msg) {
+  {
+    std::unique_lock lck(mtx_);
+    productor_.wait(lck, [this]() { return deque_.size() < capacity_; });
+    deque_.push_front(std::forward(msg));
+  }
+  consumer_.notify_one();
+}
+
+template <typename T> void blockqueue<T>::pop_front(T &msg) {
+  {
+    std::unique_lock lck(mtx_);
+    consumer_.wait(lck, [this]() { return !deque_.empty(); });
+    msg = std::move(deque_.front());
+    deque_.pop_front();
+  }
+  productor_.notify_one();
+}
